3694a19c0da90cf6da1f54a3398c76d511982e9b
Zeynel Şahin <zynl.shn@hotmail.com>
2022-08-04 08:54:38 +0300

    XML format çıktısı ve dosya indirme

---
+++ CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers;
+
+[ApiController]
+[Route("api/files")]
+public class FilesController : Controller
+{
+    private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+    public FilesController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+    {
+        _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider ?? throw new System.ArgumentNullException(nameof(fileExtensionContentTypeProvider));
+    }
+
+    [HttpGet("{fileId}")]
+    public IActionResult GetFile(string fileId)
+    {
+        var pathToFile = "ekrancıksı.PNG";
+        if (!System.IO.File.Exists(pathToFile))
+        {
+            return NotFound();
+        }
+
+        if (!_fileExtensionContentTypeProvider.TryGetContentType(pathToFile, out var contentType))
+        {
+            contentType = "application/octet-stream";
+        }
+
+        var bytes = System.IO.File.ReadAllBytes(pathToFile);
+        return File(bytes, contentType, Path.GetFileName(pathToFile));
+    }
+}
--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,13 +1,16 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 // builder.Services.AddMvc(); MVC hizmetleri
 // builder.Services.AddControllersWithViews();// Controllerları viewler ile birlikte kulllanma
-builder.Services.AddControllers(); 
+builder.Services.AddControllers(options => { options.ReturnHttpNotAcceptable = true; }).AddXmlDataContractSerializerFormatters();//Desteklenmyen formatlar için default format da verinin gönderilmemesi için
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 var app = builder.Build();
 
@@ -27,5 +30,5 @@ app.MapControllers();
 // {
 //     await context.Response.WriteAsync("Zeynel");
 // });
- 
+
 app.Run();
